8687a5fa211b4f0fe049645048b006c033a4c778
James Polley <jp@jamezpolley.com>
2015-08-12 16:14:40 +1000

    test env markers directly in install_requires

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 import setuptools
+
 setuptools.setup(
-    setup_requires=['pbr'],
-    pbr=True,
+    setup_requires = [
+        "setuptools>=17",
+    ],
+    install_requires = [
+        "setuptools>=17; python_version == '2.4'"
+    ]
 )
